3df52ce997e2043feaeeeac7addcffc360630f6c
femessina <fe.messina@reply.it>
2022-10-16 18:05:36 +0200

    Primo BE Pulito

---
+++ BE/objects/Cliente.js
@@ -0,0 +1,12 @@
+  class  Cliente{
+    constructor(Id,Denominazione,CategoriaCliente,Note,Cap){
+      this.Id = Id;
+      this.Denominazione = Denominazione;
+      this.CategoriaCliente = CategoriaCliente;
+      this.Note = Note;
+      this.Cap = Cap;
+    }
+  }
+  
+  module.exports = Cliente;
+  
